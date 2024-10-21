471b77b0ec534f32c28048b628312057ad67a848
SaLiL-SinGh14 <salilsingh02@gmail.com>
2024-10-21 09:12:49 +0530

    javascript

--- Day6/script.js
+++ Day6/script.js
@@ -1,11 +1,20 @@
-let arr = [1, 23, 4, 55, 6, 54]
-let newary = arr.map((x) => x * 2);
-console.log(newary);
-let even = arr.filter((x) => x % 2 == 0);
-console.log(even);
-let sum = arr.reduce((x, y) => x + y, 0);
-console.log(sum);
-let result = arr
-    .filter((x) => x % 2 == 0)
-    .reduce((x, y) => x + y, 0);
-console.log(result);
+// let arr = [1, 23, 4, 55, 6, 54]
+// let newary = arr.map((x) => x * 2);
+// console.log(newary);
+// let even = arr.filter((x) => x % 2 == 0);
+// console.log(even);
+// let sum = arr.reduce((x, y) => x + y, 0);
+// console.log(sum);
+// let result = arr
+//     .filter((x) => x % 2 == 0)
+//     .reduce((x, y) => x + y, 0);
+// console.log(result);
+const students =[
+    {name:'Alice',score:50 },
+    {name:'Bob',score:65 },
+    {name:'Charlie',score:80 },
+    {name:'David',score:45 },
+    {name:'Joe',score:75 },
+];
+let newarr=students.filter(students=>students.score>60) .map(students=>students.score*2) .reduce((x,y)=>x+y,0);
+console.log(newarr)
