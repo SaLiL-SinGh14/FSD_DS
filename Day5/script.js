c1e5233dd48873424340bf3c12007cea0aa1a72a
SaLiL-SinGh14 <salilsingh02@gmail.com>
2024-10-21 08:55:58 +0530

    javascript1

--- Day5/script.js
+++ Day5/script.js
@@ -16,15 +16,37 @@
 // let a;
 // let b=null;
 // console.log(obj.id);
-function validate(){
-    let un=document.getElementById('un').value;
-    let pass=document.getElementById('pass').value;
-    //we can check whether un and pass is fetching the value by using alert
-    if(un== "admin" && pass =="admin"){
-        document.getElementById("res").innerHTML="Login Success!";
-    }
-    else{
-        document.getElementById("res").innerHTML="Login Failed!";
-    }
 
+
+//**JS validation code***
+// function validate(){
+//     let un=document.getElementById('un').value;
+//     let pass=document.getElementById('pass').value;
+//     //we can check whether un and pass is fetching the value by using alert
+//     if(un== "admin" && pass =="admin"){
+//         document.getElementById("res").innerHTML="Login Success!";
+//     }
+//     else{
+//         document.getElementById("res").innerHTML="Login Failed!";
+//     }
+
+// }
+// let x=Number('12');
+// let str=Number("123");
+// console.log('12'+'3');
+// let a=12;
+// let b='12';
+// console.log(a==b);
+// console.log(a===b);
+// let a= (x,y) => {
+//     return(x+y);
+// };
+// let x=a(55,6666);
+// console.log(x)
+let say=()=> console.log("hello");
+say();
+function display(){
+    let p1 = document.querySelector('p');
+    p1.innerHTML="Result";
+    
 }
